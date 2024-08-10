ee776e0b33f9e5aba1106bc4dac3e20e520cbc2f
Virat Jaiswal <vinayjais98@gmail.com>
2024-08-10 17:50:12 +0530

    Db connection

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import dotenv from "dotenv"
+import { connectDB } from "./db/index.js"
+import express from "express";
+
+dotenv.config(
+    {
+        path: "./.env"
+    }
+)
+
+const app = express()
+
+
+connectDB()
+.then( () => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Mongodbserver is running at port ${process.env.PORT}`);
+    })
+})
+.catch( (error) => {
+    console.log("MongoDB Connection failed...", error);    
+})
